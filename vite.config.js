9e5ad71b7be1bc423b30f00c3ebd16ccf9d63ed1
Rahul0001k <jakharrahul525@gmail.com>
2026-06-23 14:41:30 +0530

    fix paths

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     ViteImageOptimizer({
+//       jpeg: { quality: 85, mozjpeg: true },
+//       jpg:  { quality: 85, mozjpeg: true },
+//       webp: { quality: 85 },
+//     }),
+//   ],
+//   base: '/'
+// })
+
+
+
 export default defineConfig({
+  base: './',
   plugins: [
     react(),
-    ViteImageOptimizer({
-      jpeg: { quality: 85, mozjpeg: true },
-      jpg:  { quality: 85, mozjpeg: true },
-      webp: { quality: 85 },
-    }),
+    ViteImageOptimizer()
   ],
-  base: '/'
 })
