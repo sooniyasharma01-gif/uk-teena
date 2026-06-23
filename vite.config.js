18b861e62a555baab31ba9f622fcd4ba9bec2a94
Rahul0001k <jakharrahul525@gmail.com>
2026-06-23 14:59:34 +0530

    fix github pages routing

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,31 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
+
+// // export default defineConfig({
+// //   plugins: [
+// //     react(),
+// //     ViteImageOptimizer({
+// //       jpeg: { quality: 85, mozjpeg: true },
+// //       jpg:  { quality: 85, mozjpeg: true },
+// //       webp: { quality: 85 },
+// //     }),
+// //   ],
+// //   base: '/'
+// // })
+
+// export default defineConfig({
+//   base: '/',
+//   plugins: [
+//     react(),
+//     ViteImageOptimizer()
+//   ],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
-
-// export default defineConfig({
-//   plugins: [
-//     react(),
-//     ViteImageOptimizer({
-//       jpeg: { quality: 85, mozjpeg: true },
-//       jpg:  { quality: 85, mozjpeg: true },
-//       webp: { quality: 85 },
-//     }),
-//   ],
-//   base: '/'
-// })
 
 export default defineConfig({
   base: '/',
@@ -21,4 +34,3 @@ export default defineConfig({
     ViteImageOptimizer()
   ],
 })
-
